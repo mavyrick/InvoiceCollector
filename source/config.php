66259e627475f107374afe9b28eb32ab25d70cb3
mavyrick <jrsorokin@gmail.com>
2017-05-10 18:39:33 -0700

    test connection 10

--- source/config.php
+++ source/config.php
@@ -13,12 +13,12 @@ include_once("source/database.class.php");
 
 function get_db_conn() {
 
-$url = parse_url(getenv("mysql://b89d1d0d390de1:cf33d27f@us-cdbr-iron-east-03.cleardb.net/heroku_f99b13abac46a60?reconnect=true"));
+$url = parse_url(getenv("DATABASE_URL"));
 
-$server = $url["us-cdbr-iron-east-03.cleardb.net"];
-$username = $url["b89d1d0d390de1"];
-$password = $url["cf33d27f"];
-$db = substr($url["heroku_f99b13abac46a60"], 1);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
 // $conn = new mysqli($server, $username, $password, $db);
 $conn = HowardSQL::Connect($server, $username, $password, $db);
