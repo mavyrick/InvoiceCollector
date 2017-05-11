9bc0f7b2ddaece31726243879f035b0503579271
mavyrick <jrsorokin@gmail.com>
2017-05-10 17:55:34 -0700

    test connection 6

--- source/config.php
+++ source/config.php
@@ -1,5 +1,4 @@
-<?
-
+<!--
 	// include_once("source/database.class.php");
 	//
 	// function get_db_conn() {
@@ -7,10 +6,10 @@
 	// 	// 'Location' , 'User' , 'Password' , 'Database'
 	// 		$db = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'b89d1d0d390de1', 'cf33d27f'); //Create new connection
 	// 		return $db; //Return the database connection
-	// }
-
-?>
+	// } -->
 <?php
+
+function get_db_conn() {
 
 $url = parse_url(getenv("mysql://b89d1d0d390de1:cf33d27f@us-cdbr-iron-east-03.cleardb.net/heroku_f99b13abac46a60?reconnect=true"));
 
@@ -19,6 +18,8 @@ $username = $url["b89d1d0d390de1"];
 $password = $url["cf33d27f"];
 $db = substr($url["heroku_f99b13abac46a60"], 1);
 
-$sql = new mysqli($server, $username, $password, $db);
+$conn = new mysqli($server, $username, $password, $db);
+
+return $conn;
 
 ?>
