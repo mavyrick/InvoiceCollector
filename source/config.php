acbfdbd4c4000205179a04836893edd09f812281
mavyrick <jrsorokin@gmail.com>
2017-05-10 17:15:47 -0700

    test connection

--- source/config.php
+++ source/config.php
@@ -5,7 +5,7 @@
 	function get_db_conn() {
 		//These are the settings for your database connection
 		// 'Location' , 'User' , 'Password' , 'Database'
-			$db = mysqli_connect('localhost', 'us-cdbr-iron-east-03.cleardb.net', 'b89d1d0d390de1', 'cf33d27f'); //Create new connection
+			$db = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'b89d1d0d390de1', 'cf33d27f'); //Create new connection
 			return $db; //Return the database connection
 	}
 
