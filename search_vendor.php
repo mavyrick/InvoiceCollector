46d283fc79ddbced3bee069ba71a02038987e388
Josh Sorokin <jrsorokin@gmail.com>
2019-07-02 15:57:47 +0300

    Fix indentations and remove duplicated files

--- search_vendor.php
+++ search_vendor.php
@@ -13,23 +13,23 @@ if(isset($_POST["hidden_vendors"]) && strlen($_POST["hidden_vendors"])>0)
   if ($stmt = $sql->prepare("SELECT * FROM `invoices` WHERE `vendor` = '$search_save'") AND $stmt->execute(array()) AND $data = $stmt->fetchAll()) {
     foreach ($data as $i) {
 
-    $total_save = $i["subtotal"] + ($i["pst"]/100 * $i["subtotal"]) + ($i["gst"]/100 * $i["subtotal"]);
+        $total_save = $i["subtotal"] + ($i["pst"]/100 * $i["subtotal"]) + ($i["gst"]/100 * $i["subtotal"]);
 
-    echo "<tr id=" . "row_" . $i["id"] . ">";
-    echo "<td>" . $i["invoice_date"] . "</td>";
-    echo "<td>" . $i["invoice_number"] . "</td>";
-    echo "<td>" . $i["vendor"] . "</td>";
-    echo "<td>" . "$" . number_format((float)$i["subtotal"], 2, '.', '') . "</td>";
-    echo "<td>" . "$" . number_format((float)$total_save, 2, '.', '') . "</td>";
-    echo "<td>" . "<input type='submit' value='Delete' class='del_button btn btn-danger btn-sm' id=" . $i["id"] . ">" . "</td>";
-    echo "</tr>";
-
-    }
-
+        echo "<tr id=" . "row_" . $i["id"] . ">";
+        echo "<td>" . $i["invoice_date"] . "</td>";
+        echo "<td>" . $i["invoice_number"] . "</td>";
+        echo "<td>" . $i["vendor"] . "</td>";
+        echo "<td>" . "$" . number_format((float)$i["subtotal"], 2, '.', '') . "</td>";
+        echo "<td>" . "$" . number_format((float)$total_save, 2, '.', '') . "</td>";
+        echo "<td>" . "<input type='submit' value='Delete' class='del_button btn btn-danger btn-sm' id=" . $i["id"] . ">" . "</td>";
+        echo "</tr>";
 
     }
 
-    $stmt = null;
-  }
+
+}
+
+$stmt = null;
+}
 
 ?>
--- vendor_response.php
+++ vendor_response.php
@@ -25,19 +25,19 @@ $sql = get_db_conn();
 
 
 if(isset($_POST["vendor_title"]) && strlen($_POST["vendor_title"])>0)
-if(isset($_POST["address"]) && strlen($_POST["address"])>0)
-{
+  if(isset($_POST["address"]) && strlen($_POST["address"])>0)
+  {
 
-  $vendor_title_save = filter_var($_POST["vendor_title"],FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-  $address_save = filter_var($_POST["address"],FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    $vendor_title_save = filter_var($_POST["vendor_title"],FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    $address_save = filter_var($_POST["address"],FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
 
-  if ($stmt = $sql->prepare("SELECT * FROM `vendors` ORDER BY `id` DESC LIMIT 1") AND $stmt->execute(array()) AND $data = $stmt->fetch()) {
+    if ($stmt = $sql->prepare("SELECT * FROM `vendors` ORDER BY `id` DESC LIMIT 1") AND $stmt->execute(array()) AND $data = $stmt->fetch()) {
 
-    $id_save = $data["id"];
+      $id_save = $data["id"];
 
-  }
+    }
 
-  $stmt = null;
+    $stmt = null;
 
 
   //  {
@@ -60,7 +60,7 @@ if(isset($_POST["address"]) && strlen($_POST["address"])>0)
   //         exit();
   // }
 
-  $insert_row = $sql->query("INSERT INTO `vendors` (vendor_title, address) VALUES ('$vendor_title_save', '$address_save')");
+    $insert_row = $sql->query("INSERT INTO `vendors` (vendor_title, address) VALUES ('$vendor_title_save', '$address_save')");
 
   // $stmt->bindParam(":ename", $invoice_date_save);
   // $stmt->bindParam(":edept", $invoice_number_save);
@@ -71,27 +71,27 @@ if(isset($_POST["address"]) && strlen($_POST["address"])>0)
   // $stmt->bindParam(":ename", $total);
   // $stmt->bindParam(":edept", $total_save);
 
-  if($insert_row)
-  {
+    if($insert_row)
+    {
 
-    header('Content-Type: application/json');
-
-
-    echo json_encode(array( 'vendor_title' => $vendor_title_save,
-    'address' => $address_save,
-    'id' => $id_save,
-  ));
+      header('Content-Type: application/json');
 
 
-  $my_id = $sql->insert_id;
+      echo json_encode(array( 'vendor_title' => $vendor_title_save,
+        'address' => $address_save,
+        'id' => $id_save,
+      ));
 
-  exit();
 
-}else{
+      $my_id = $sql->insert_id;
 
-  header('HTTP/1.1 500 Looks like mysql error, could not insert record!');
-  exit();
-}
+      exit();
+
+    }else{
+
+      header('HTTP/1.1 500 Looks like mysql error, could not insert record!');
+      exit();
+    }
 
 // if ($stmt = $sql->prepare("SELECT `invoice_number` FROM `invoices` WHERE `invoice_number` = $invoice_number_save LIMIT 2") AND $stmt->execute(array("1")) AND $data = $stmt->fetch()) {
 //
@@ -136,23 +136,23 @@ if(isset($_POST["address"]) && strlen($_POST["address"])>0)
 // echo $count;
 
 
-}
-
-if(isset($_POST["vendorToDelete"]) && strlen($_POST["vendorToDelete"])>0 && is_numeric($_POST["vendorToDelete"]))
-{
-
-
-  $idToDelete = filter_var($_POST["vendorToDelete"],FILTER_SANITIZE_NUMBER_INT);
-
-  $delete_row = $sql->query("DELETE FROM `vendors` WHERE `id` = $idToDelete");
-
-  if(!$delete_row)
-  {
-
-    header('HTTP/1.1 500 Could not delete record!');
-    exit();
-
   }
 
-}
-?>
+  if(isset($_POST["vendorToDelete"]) && strlen($_POST["vendorToDelete"])>0 && is_numeric($_POST["vendorToDelete"]))
+  {
+
+
+    $idToDelete = filter_var($_POST["vendorToDelete"],FILTER_SANITIZE_NUMBER_INT);
+
+    $delete_row = $sql->query("DELETE FROM `vendors` WHERE `id` = $idToDelete");
+
+    if(!$delete_row)
+    {
+
+      header('HTTP/1.1 500 Could not delete record!');
+      exit();
+
+    }
+
+  }
+  ?>
